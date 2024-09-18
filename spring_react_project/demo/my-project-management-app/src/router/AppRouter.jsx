6f4fe2487595843f45ca5518b01f3408462e9dd7
napppkun <napaphat.khun@gmail.com>
2024-09-18 15:31:34 +0700

    remove unused page

--- spring_react_project/demo/my-project-management-app/src/router/AppRouter.jsx
+++ spring_react_project/demo/my-project-management-app/src/router/AppRouter.jsx
@@ -20,13 +20,13 @@ function AppRouter(){
             <Route path="/admin/project-crud" element = {<ProjectCrud />} />
             <Route path="/admin/course-crud" element = {<CourseCrud />} />
             <Route path="/admin/supervisor-crud" element = {<SupervisorCrud />} />
-            <Route path="/user" element = {<UserPage />} />
             <Route path="/home-page" element = {<HomePage />} /> 
             <Route path="/user/std-profile" element = {<StdProfilePage />} />  
             <Route path="/user/std-edit-profile" element = {<StdEditProfilePage />} />
             <Route path="/user/std-add-project" element = {<StdAddProjectPage />} />  
             <Route path="/user/std-manage-project" element = {<StdManageProjectPage />} /> 
             <Route path="/admin/profile" element = {<AdminProfilePage />} />  
+            
         </Routes>   
     </Router>
   );
